28bbd400d62269d91e9b1f74a761a1827670dba0
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-10-23 14:52:45 -0500

    added json server to store journal entries and api to fetch entries.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -7,29 +7,7 @@
  */
 
 // This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "10/10/2020",
-        concept: "Data Provider",
-        entry: "I learned how to create a data provider file in js to hold the data I want to show as HTML in the DOM.",
-        mood: "Overwhelmed"
-    },
-    {
-        id: 3,
-        date: "10/15/2020",
-        concept: "List Files",
-        entry: "I learned how to create a list file in js to hold the HTML representatins of the js that I want to show",
-        mood: "Stressed"
-    }
-]
+let journal = []
 
 /*
     You export a function that provides a version of the
@@ -41,4 +19,12 @@ export const useJournalEntries = () => {
             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
     )
     return sortedByDate
+}
+
+export const getEntries = () => {
+    return fetch("http://localhost:8088/journal")
+    .then(response => response.json())
+    .then(parsedEntries => {
+        journal = parsedEntries
+    })
 }
