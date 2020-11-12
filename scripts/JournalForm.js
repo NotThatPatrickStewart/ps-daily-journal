d150bf6baf53564c1650c1f98a24e2f566d6b9ee
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-12 15:59:29 -0600

    added .map iteration for mood.id

--- scripts/JournalForm.js
+++ scripts/JournalForm.js
@@ -1,15 +1,16 @@
 import { saveEntry } from './JournalDataProvider.js'
+import { getMood, useMood } from './MoodProvider.js'
 
 const contentTarget = document.querySelector(".container__left")
 const eventHub = document.querySelector(".container")
 
-const render = () => {
+const render = (moodCollection) => {
     contentTarget.innerHTML = `
     <h2>Today's Concepts</h2>
         <textarea id="journalConcepts" placeholder="What concepts did you cover?" cols="50" rows="3"></textarea>
     <h2>Today's Entry</h2>
         <textarea id="journalEntry" placeholder="Let's go into a little more detail" cols="100" rows="20"></textarea>
-    <h2>Today's Mood</h2>
+        <h2>Today's Mood</h2>
         <select class="dropdown" id="moodDropdown">
             <option value="pickOne">Please Select a Mood</option>
             <option value="Stressed">Stressed</option>
@@ -18,9 +19,16 @@ const render = () => {
             <option value="Freaking Out">Freaking Out</option>
             <option value="Actually Not That Bad!">Actually Not That Bad!</option>
         </select>
+        <div>
+        ${
+            moodCollection.map(
+                (mood) => {
+                    return `<option value="${mood.id}">${mood.label}</option>`
+                }
+            ).join("")
+        }
+</div>
     <button id="saveEntry">Yeet</button>
-    <button id="editEntry">Edit</button>
-    <button id="deleteEntry">Delete</button>
     `
 }
 
@@ -28,16 +36,16 @@ eventHub.addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "saveEntry") {
     console.log("clickEvent", clickEvent)
 
-    const timestamp = Date.now()
-    const journalConcepts = document.querySelector("#journalConcepts").value
-    const journalEntry = document.querySelector("#journalEntry").value
-    const moodDropdown = document.querySelector("#moodDropdown").value
+    const moodId = parsInt(document.querySelector("#moodId").value)
+    const date = Date.now()
+    const concepts = document.querySelector("#concepts").value
+    const entry = document.querySelector("#entry").value
     
     const newEntry = {
-        timestamp,
-        journalConcepts,
-        journalEntry,
-        moodDropdown
+        moodId,
+        date,
+        concepts,
+        entry
     }
 
     saveEntry(newEntry)
@@ -45,5 +53,8 @@ eventHub.addEventListener("click", clickEvent => {
 })
 
 export const JournalForm = () => {
-    render()
+    getMood().then(() => {
+        const allMoods = useMood()
+        render(allMoods)
+    })
 }
